dad071f57424cc726786df4a9efeb7f42aa0420c
45253 <45253@10.18.27.25>
2020-03-22 18:02:39 +0100

    Initial commit of the project

---
+++ Calculator/src/Addition.java
@@ -0,0 +1,14 @@
+public class Addition extends Operation{
+	@Override 
+	public String getName() {
+		return "addition";
+	}
+	@Override
+	public String getOperator() {
+		return "+";
+	}
+	@Override
+	public double calc() {
+		return a+b;
+	}
+}
